52fea82d425fddcde81619ffc75cd7b0ffbe80c7
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-11-05 15:08:34 +0200

    Add: compomenta edit

--- src/components/EditQuestionnaire/EditQuestionnaire.styled.jsx
+++ src/components/EditQuestionnaire/EditQuestionnaire.styled.jsx
@@ -1,1 +1,125 @@
 import styled from 'styled-components';
+
+export const ButtonBack = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  gap: 10px;
+  margin-bottom: 13px;
+`;
+
+export const EditContainer = styled.div``;
+
+export const Title = styled.h2`
+  margin-bottom: 8px;
+
+  font-size: 25.6px;
+  font-weight: 700;
+  line-height: 38px;
+`;
+
+export const SubTitle = styled.p`
+  margin-bottom: 32px;
+
+  font-size: 14.2px;
+  font-weight: 400;
+  line-height: 21px;
+`;
+
+export const LabelQuestionnaire = styled.p`
+  margin-bottom: 17px;
+
+  font-size: 14.2px;
+  font-weight: 400;
+  line-height: 18px;
+`;
+
+export const ContainerQuestionnaire = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  height: 80px;
+
+  padding: 16px;
+
+  border-radius: 8px 8px 0px 0px;
+  border: 1px solid #e3e3e3;
+  background-color: #f9f9f9;
+  border-radius: 10px;
+`;
+
+export const ContainerInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ContainerIcon = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+`;
+
+export const InfoText = styled.p`
+  font-size: 14.2px;
+  font-weight: 500;
+  line-height: 18px;
+`;
+
+export const InfoData = styled.p`
+  font-size: 11.2px;
+  font-weight: 400;
+  line-height: 14.1px;
+`;
+
+export const ContainerInfoAvatar = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 30px;
+`;
+
+export const QuestionContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const QuestionInfo = styled.div`
+  display: flex;
+  margin-top: 16px;
+  margin-bottom: 8px;
+
+  padding: 15px 24px;
+
+  width: 100%;
+  height: 48px;
+  border-radius: 10px;
+  background-color: #fff;
+`;
+
+export const QuestionText = styled.p`
+  margin-left: 40px;
+  font-size: 14.2px;
+  font-weight: 400;
+  line-height: 18px;
+`;
+
+export const ContainerOptions = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+
+  margin-left: auto;
+`;
+
+export const Count = styled.p`
+  font-size: 14.2px;
+  font-weight: 400;
+  line-height: 18px;
+
+  color: #8e8e8e;
+`;
+
+export const Box = styled.div`
+  background-color: #fff;
+  border-radius: 10px;
+`;
