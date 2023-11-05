9a3728399f781a4067299bd070ae42ca59712ff2
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-11-05 09:03:27 +0200

    update routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../pages/LoginPage/LoginPage';
 import { DashboardPage } from '../pages/DashboardPage/DashboardPage';
 import { QuizGame } from '../components/QuizGame/ContentblockGame';
+import { EditQuestionnaire } from '../components/EditQuestionnaire/EditQuestionnaire';
 
 export const AppRoutes = () => {
   return (
@@ -11,9 +12,9 @@ export const AppRoutes = () => {
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} >
-            <Route path='edit' element={''}/>
-            <Route path='game' element={<QuizGame/>}/>
+          <Route path="/dashboard" element={<DashboardPage />}>
+            <Route path="edit" element={<EditQuestionnaire />} />
+            <Route path="game" element={<QuizGame />} />
           </Route>
         </Routes>
       </Suspense>
